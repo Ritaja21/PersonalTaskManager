1653b4bdba56601a6c20d7ebdeba15f451076198
Ritaja <tonnitarafder2004@gmail.com>
2025-06-23 12:43:18 +0530

    WIP: save changes before rebase

--- Task-Manager/app.js
+++ Task-Manager/app.js
@@ -1,19 +1,12 @@
+// app.js
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
-
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/tasks', taskRoutes);
 
-
- app.use('/api/tasks', taskRoutes);
-
-const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-    .catch(err => console.log(err));
+module.exports = app; // <-- Export app for Supertest
